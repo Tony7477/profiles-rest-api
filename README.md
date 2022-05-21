6066bcabc5430570160e35c0409688ff3fbb6176
hariharan <hariharan7477@gmail.com>
2022-05-21 23:08:06 +0530

    addes hello api view

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 #profiles Rest Api
-hi this is hariharan currently learning django ,this is just purely for my career improvement.
+hi this is hariharan, currently learning Django ,this is just purely for my career improvement just for development purposes in
+python.
+
+When to use api views?
+->Need full control over Logic
+->processing files and rendering synchronous response
+->you are calling other API's/services
+->access local files or data
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+
 
 # Create your views here.
+class HelloApiView(APIView):
+
+    """Test API VIEW"""
+    def get(self, request, format=None):
+        """Resturns a list of APIView features"""
+        an_apiview=[
+        'Uses HTTP methods as functions(get,post,put,delete,patch)',
+        'Is similar to a traditional Django View',
+        'Gives you the most control over you applications logic',
+        'Is mapped manually to URLS',
+        ]
+        return Response({'message':'Hello!','an_apiview':an_apiview})
