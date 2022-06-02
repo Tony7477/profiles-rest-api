b583d6fc3974330c84af1ae13b51d87974a0e5bc
hariharan <hariharan7477@gmail.com>
2022-06-02 23:25:37 +0530

    serializer

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,13 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
 
 
 
 # Create your views here.
 class HelloApiView(APIView):
-
     """Test API VIEW"""
+    serializer_class=serializers.HelloSerializer
+
     def get(self, request, format=None):
         """Resturns a list of APIView features"""
         an_apiview=[
@@ -17,3 +20,19 @@ class HelloApiView(APIView):
         'Is mapped manually to URLS',
         ]
         return Response({'message':'Hello!','an_apiview':an_apiview})
+
+    def post(self,request):
+        """hello message with our name"""
+        serializer=self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            message=f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(
+
+            serializer.errors,
+            status=status.HTTP_404_BAD_REQUEST
+            
+
+            )
